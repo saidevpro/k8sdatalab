2b9108596c841da8e3791e1c233f1605f27d21ae
Said Sow <saidevproject@gmail.com>
2026-04-21 15:14:03 +0000

    fix: use sparksubmitoperator

--- src/airflow/dags/ingestion_raw_data.py
+++ src/airflow/dags/ingestion_raw_data.py
@@ -1,34 +1,45 @@
-from airflow import DAG
-from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
 from datetime import datetime
+
+from airflow import DAG
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 
 with DAG(
     dag_id="ingestion_raw_data",
     start_date=datetime(2026, 1, 1),
     schedule="@daily",
-    catchup=False
+    catchup=False,
 ) as dag:
-    ingest_task = SparkKubernetesOperator(
+    ingest_task = SparkSubmitOperator(
         task_id="ingest_prim_idfm_dataset",
-        namespace="spark-jobs",
-        application_file="/app/spark/confs/spark-small.conf",
-        image="saidevpro/spark:3.5.8-jobs",
-        app_name="ingestion-prim-idfm",
-        main_application_file="local:///opt/spark/jobs/bronze/ingestion_prim_idfm_dataset.py",
-        spark_conf={
-            "spark.kubernetes.container.image": "saidevpro/spark:3.5.8-jobs",
-            "spark.kubernetes.namespace": "spark-jobs",
-            "spark.kubernetes.authenticate.driver.serviceAccountName": "spark",
-            "spark.master": "k8s://https://kubernetes.default.svc:443",
-            "spark.submit.deployMode": "cluster",
-            "spark.sql.catalog.nessie.ref": "dev",
-            "spark.sql.catalog.nessie.uri": "http://nessie.k8sdatalab.com/api/v1",
-            "spark.hadoop.fs.s3a.access.key": "{{ var.value.MINIO_ACCESS_KEY }}",
-            "spark.hadoop.fs.s3a.secret.key": "{{ var.value.MINIO_SECRET_KEY }}"
-        },
+        conn_id="spark_default",
+        application="local:///opt/spark/jobs/bronze/ingestion_prim_idfm_dataset.py",
+        name="ingestion-prim-idfm",
+        deploy_mode="cluster",
         env_vars={
             "NESSIE_NAMESPACE": "bronze",
             "PRIM_DATASET_URI": "{{ var.value.PRIM_DATASET_URI }}",
-            "DESTINATION_TABLE": "nessie.bronze.accessibility_gares"
-        }
+            "DESTINATION_TABLE": "nessie.bronze.accessibility_gares",
+        },
+        conf={
+            "spark.master": "k8s://https://kubernetes.default.svc:443",
+            "spark.kubernetes.container.image": "saidevpro/spark:3.5.8-jobs",
+            "spark.kubernetes.namespace": "spark-jobs",
+            "spark.kubernetes.authenticate.driver.serviceAccountName": "spark",
+            "spark.submit.deployMode": "cluster",
+
+            "spark.sql.catalog.nessie.ref": "dev",
+            "spark.sql.catalog.nessie.uri": "http://nessie.k8sdatalab.com/api/v1",
+
+            "spark.hadoop.fs.s3a.access.key": "{{ var.value.MINIO_ACCESS_KEY }}",
+            "spark.hadoop.fs.s3a.secret.key": "{{ var.value.MINIO_SECRET_KEY }}",
+
+            "spark.kubernetes.driverEnv.NESSIE_NAMESPACE": "bronze",
+            "spark.kubernetes.driverEnv.PRIM_DATASET_URI": "{{ var.value.PRIM_DATASET_URI }}",
+            "spark.kubernetes.driverEnv.DESTINATION_TABLE": "nessie.bronze.accessibility_gares",
+        },
+        num_executors=1,
+        executor_cores=1,
+        executor_memory="1g",
+        driver_memory="512m",
+        verbose=True,
     )
