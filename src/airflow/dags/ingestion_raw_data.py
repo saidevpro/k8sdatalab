a20426e979dd799d6aa71c4ae3537563e56d2fec
Said Sow <saidevproject@gmail.com>
2026-04-22 14:07:21 +0000

    feat: add spark properties file to ingestion dag

--- src/airflow/dags/ingestion_raw_data.py
+++ src/airflow/dags/ingestion_raw_data.py
@@ -15,6 +15,7 @@ with DAG(
         application="local:///opt/spark/jobs/bronze/ingestion_prim_idfm_dataset.py",
         name="ingestion-prim-idfm",
         deploy_mode="cluster",
+        properties_file="/app/spark/confs/spark-small.conf",
         env_vars={
             "NESSIE_NAMESPACE": "bronze",
             "PRIM_DATASET_URI": "{{ var.value.PRIM_DATASET_URI }}",
@@ -24,10 +25,8 @@ with DAG(
             "spark.kubernetes.container.image": "saidsow/spark:3.5.8-jobs",
             "spark.kubernetes.namespace": "spark-jobs",
             "spark.kubernetes.authenticate.driver.serviceAccountName": "spark",
-
             "spark.sql.catalog.nessie.ref": "dev",
             "spark.sql.catalog.nessie.uri": "http://nessie.k8sdatalab.com/api/v1",
-
             "spark.hadoop.fs.s3a.access.key": "{{ var.value.MINIO_ACCESS_KEY }}",
             "spark.hadoop.fs.s3a.secret.key": "{{ var.value.MINIO_SECRET_KEY }}",
         },
