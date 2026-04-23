04d16fab5a99b73581e7ce00b187db3d6666c57d
Said Sow <saidevproject@gmail.com>
2026-04-23 17:09:54 +0000

    fix: setup nessie catalog

--- src/spark/jobs/setup_nessie_catalog.py
+++ src/spark/jobs/setup_nessie_catalog.py
@@ -2,11 +2,11 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.getOrCreate()
 
-branches = ["main", "dev", "uat"]
+branches = ["main", "dev"]
 namespaces = ["bronze", "silver", "gold"]
 
 for branch in branches:
-    spark.conf.set("spark.sql.catalog.nessie.ref", branch)
+    spark.sql(f"USE REFERENCE {branch} IN nessie")
     for namespace in namespaces:
         spark.sql(f"CREATE NAMESPACE IF NOT EXISTS nessie.{namespace}")
         
