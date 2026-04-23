d4db53987421652d614f5795d816169c430a5ad8
Said Sow <saidevproject@gmail.com>
2026-04-23 20:17:01 +0000

    bootstrap nessie create branch

--- src/spark/jobs/setup_nessie_catalog.py
+++ src/spark/jobs/setup_nessie_catalog.py
@@ -6,6 +6,7 @@ branches = ["main", "dev"]
 namespaces = ["bronze", "silver", "gold"]
 
 for branch in branches:
+    spark.sql(f"CREATE BRANCH IF NOT EXISTS {branch} IN nessie FROM main")
     spark.sql(f"USE REFERENCE {branch} IN nessie")
     for namespace in namespaces:
         spark.sql(f"CREATE NAMESPACE IF NOT EXISTS nessie.{namespace}")
