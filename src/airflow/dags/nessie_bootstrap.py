2ec136b150af5a5dcf032e552210747aeab9d891
Said Sow <saidevproject@gmail.com>
2026-04-23 11:23:41 +0000

    feat: add dag nessie bootstrap

---
+++ src/airflow/dags/nessie_bootstrap.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+
+from airflow import DAG
+from airflow.providers.standard.operators.bash import BashOperator
+
+with DAG(
+    dag_id="bootstrap_nessie_namespaces",
+    start_date=datetime(2026, 1, 1),
+    schedule=None,
+    catchup=False,
+    tags=["bootstrap", "nessie"],
+) as dag:
+
+    ensure_nessie_namespaces = BashOperator(
+        task_id="ensure_nessie_namespaces_main_and_dev",
+        bash_command="""
+        set -e
+
+        nessie sql <<'SQL'
+        CONNECT TO http://nessie.data-platform.svc.cluster.local:19120/api/v2 ON main;
+        CREATE NAMESPACE IF NOT EXISTS bronze;
+        CREATE NAMESPACE IF NOT EXISTS silver;
+        CREATE NAMESPACE IF NOT EXISTS gold;
+
+        CONNECT TO http://nessie.data-platform.svc.cluster.local:19120/api/v2 ON dev;
+        CREATE NAMESPACE IF NOT EXISTS bronze;
+        CREATE NAMESPACE IF NOT EXISTS silver;
+        CREATE NAMESPACE IF NOT EXISTS gold;
+        SQL
+        """,
+    )
