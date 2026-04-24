edcd68c0ed073728e205bb9ba74e5be14c33c4bf
Said Sow <saidevproject@gmail.com>
2026-04-24 09:53:20 +0000

    feat: add gtfs ingestion spark script and airflow dag

---
+++ src/airflow/dags/gtfs_pipelines/dag_gtfs.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+import shared_lib.helpers as h
+
+dag_domain = "gtfs"
+
+with DAG(
+    dag_id=h.format_etl_dag_id(dag_domain),
+    start_date=datetime(2026, 1, 1),
+    schedule="@daily",
+    catchup=False,
+    tags=h.generate_etl_dag_tags(dag_domain),
+) as dag:
+    bronze_task = SparkSubmitOperator(
+        task_id=h.format_etl_bronze_dag_task_id(dag_domain),
+        name=h.format_etl_bronze_dag_task_name(dag_domain),
+        conn_id="spark_local",
+        application="local:///opt/spark/jobs/bronze/ingestion_gtfs_idfm_dataset.py",
+        deploy_mode="client",
+        properties_file="/app/spark/confs/spark-small.conf",
+        env_vars={
+            "PRIM_DATASET_URI": "{{ var.value.PRIM_DATASET_URI }}",
+            "PRIM_TOKEN": "{{ var.value.PRIM_TOKEN }}",
+            "MINIO_URL": "{{ var.value.MINIO_URL }}",
+            "MINIO_ACCESS_KEY": "{{ var.value.MINIO_ACCESS_KEY }}",
+            "MINIO_SECRET_KEY": "{{ var.value.MINIO_SECRET_KEY }}",
+        },
+        conf={
+            "spark.kubernetes.container.image": "saidsow/spark:3.5.8",
+            "spark.kubernetes.namespace": "spark-jobs",
+            "spark.kubernetes.authenticate.driver.serviceAccountName": "spark",
+            "spark.hadoop.fs.s3a.access.key": "{{ var.value.MINIO_ACCESS_KEY }}",
+            "spark.hadoop.fs.s3a.secret.key": "{{ var.value.MINIO_SECRET_KEY }}",
+            "spark.sql.catalog.nessie.ref": "dev",
+            "spark.sql.catalog.nessie.warehouse": "s3a://datalake/warehouse/",
+            "spark.openlineage.namespace": "bronze_ingestion",
+            "spark.openlineage.appName": h.format_etl_bronze_dag_task_name(dag_domain)
+        },
+        verbose=True,
+    )
---
+++ src/spark/jobs/bronze/ingestion_gtfs_idfm_dataset.py
@@ -0,0 +1,208 @@
+import os
+import requests
+import zipfile
+import boto3
+import io
+import secrets
+from datetime import datetime
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import current_timestamp
+
+def createOrOverwritePartitions(sparkSession, df, dest_table):
+    if not sparkSession.catalog.tableExists(dest_table):
+        (
+            df
+            .writeTo(dest_table)
+            .partitionedBy("ingestion_date")
+            .option("merge-schema", "true")
+            .create()
+        )
+    else:
+        (
+            df
+            .writeTo(dest_table)
+            .option("merge-schema", "true")
+            .overwritePartitions()
+        )
+
+nessie_catalog_namespace = "nessie.bronze"
+PRIM_DATASET_URI = os.getenv("PRIM_DATASET_URI")
+ENDPOINT="/offre-horaires-tc-gtfs-idfm/exports/json"
+PRIM_TOKEN=os.getenv("PRIM_TOKEN")
+
+headers = {
+    "Authorization": f"apikey {PRIM_TOKEN}",
+    "Accept": "text/csv",
+}
+
+r = requests.get(f"{PRIM_DATASET_URI}{ENDPOINT}", headers=headers, timeout=30)
+r.raise_for_status()
+
+res = r.json()
+
+if not len(res):
+    raise ValueError("GTFS IDFM response is empty", res)
+
+file = res[0]
+zip_path = file.get("url").get("url")
+zip_local_dir = "/tmp/gtfs_idfm.zip"
+
+resp = requests.get(zip_path, headers=headers, timeout=60, stream=True)
+resp.raise_for_status()
+zip_bytes = io.BytesIO(resp.content)
+
+tmp_bucket = "tmp-spark"
+run_id = f"{datetime.utcnow():%Y-%m-%dT%H%M%S}-{secrets.token_hex(8)}"
+s3_prefix = f"ingest/{run_id}"
+
+s3 = boto3.client(
+    "s3",
+    endpoint_url=os.getenv("MINIO_URL"),
+    aws_access_key_id=os.getenv("MINIO_ACCESS_KEY"),
+    aws_secret_access_key=os.getenv("MINIO_SECRET_KEY"),
+)
+
+uploaded = []
+with zipfile.ZipFile(zip_bytes) as zf:
+    for member in zf.namelist():
+        if not member.endswith(".txt"):
+            continue
+        with zf.open(member) as f:
+            key = f"{s3_prefix}/{member}"
+            s3.upload_fileobj(f, tmp_bucket, key)
+            uploaded.append(f"s3a://{tmp_bucket}/{key}")
+
+print(f"Uploaded {len(uploaded)} files")
+
+spark = SparkSession.builder.getOrCreate()
+
+########################### TRANSFERT ##########################
+transfers_df = (
+    spark.read
+    .option("header", "true")
+    .option("inferSchema", "true")   
+    .option("delimiter", ",")        
+    .csv(f"s3a://{tmp_bucket}/{s3_prefix}/transfers.txt")
+    .withColumn("ingestion_date", current_timestamp())
+)
+
+createOrOverwritePartitions(
+    sparkSession=spark,
+    df=transfers_df,
+    dest_table=f"{nessie_catalog_namespace}.transfers"
+)
+
+########################### CALENDARS ##########################
+calendar_df = (
+    spark.read
+    .option("header", "true")
+    .option("inferSchema", "true")   
+    .option("delimiter", ",")        
+    .csv(f"s3a://{tmp_bucket}/{s3_prefix}/calendar.txt")
+    .withColumn("ingestion_date", current_timestamp())
+)
+
+createOrOverwritePartitions(
+    sparkSession=spark,
+    df=transfers_df,
+    dest_table=f"{nessie_catalog_namespace}.calendars"
+)
+
+########################### CALENDAR DATES ##########################
+calendar_dates_df = (
+    spark.read
+    .option("header", "true")
+    .option("inferSchema", "true")   
+    .option("delimiter", ",")        
+    .csv(f"s3a://{tmp_bucket}/{s3_prefix}/calendar_dates.txt")
+    .withColumn("ingestion_date", current_timestamp())
+)
+
+createOrOverwritePartitions(
+    sparkSession=spark,
+    df=transfers_df,
+    dest_table=f"{nessie_catalog_namespace}.calendar_dates"
+)
+
+########################### STOPS ##########################
+stops_df = (
+    spark.read
+    .option("header", "true")
+    .option("inferSchema", "true")   
+    .option("delimiter", ",")        
+    .csv(f"s3a://{tmp_bucket}/{s3_prefix}/stops.txt")
+    .withColumn("ingestion_date", current_timestamp())
+)
+
+createOrOverwritePartitions(
+    sparkSession=spark,
+    df=transfers_df,
+    dest_table=f"{nessie_catalog_namespace}.stops"
+)
+
+########################### STOPS TIMES ##########################
+stop_times_df = (
+    spark.read
+    .option("header", "true")
+    .option("inferSchema", "true")   
+    .option("delimiter", ",")        
+    .csv(f"s3a://{tmp_bucket}/{s3_prefix}/stop_times.txt")
+    .withColumn("ingestion_date", current_timestamp())
+)
+
+createOrOverwritePartitions(
+    sparkSession=spark,
+    df=transfers_df,
+    dest_table=f"{nessie_catalog_namespace}.stop_times"
+)
+
+########################### TRIPS ##########################
+trips_df = (
+    spark.read
+    .option("header", "true")
+    .option("inferSchema", "true")   
+    .option("delimiter", ",")        
+    .csv(f"s3a://{tmp_bucket}/{s3_prefix}/trips.txt")
+    .withColumn("ingestion_date", current_timestamp())
+)
+
+createOrOverwritePartitions(
+    sparkSession=spark,
+    df=transfers_df,
+    dest_table=f"{nessie_catalog_namespace}.trips"
+)
+
+########################### ROUTES ##########################
+routes_df = (
+    spark.read
+    .option("header", "true")
+    .option("inferSchema", "true")   
+    .option("delimiter", ",")        
+    .csv(f"s3a://{tmp_bucket}/{s3_prefix}/routes.txt")
+    .withColumn("ingestion_date", current_timestamp())
+)
+
+createOrOverwritePartitions(
+    sparkSession=spark,
+    df=transfers_df,
+    dest_table=f"{nessie_catalog_namespace}.routes"
+)
+
+########################### PATHWAYS ##########################
+pathways_df = (
+    spark.read
+    .option("header", "true")
+    .option("inferSchema", "true")   
+    .option("delimiter", ",")        
+    .csv(f"s3a://{tmp_bucket}/{s3_prefix}/pathways.txt")
+    .withColumn("ingestion_date", current_timestamp())
+)
+
+createOrOverwritePartitions(
+    sparkSession=spark,
+    df=transfers_df,
+    dest_table=f"{nessie_catalog_namespace}.pathways"
+)
+
+
+spark.stop()
