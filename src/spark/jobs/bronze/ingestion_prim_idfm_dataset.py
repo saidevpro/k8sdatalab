5f0028ef728351a13268364c6d752b64ea0fd1ef
Said Sow <saidevproject@gmail.com>
2026-05-15 23:09:56 +0000

    feat(spark): ingestion idfm dataset script add header authorization

--- src/spark/jobs/bronze/ingestion_prim_idfm_dataset.py
+++ src/spark/jobs/bronze/ingestion_prim_idfm_dataset.py
@@ -11,15 +11,21 @@ nessie_namespace = os.getenv("NESSIE_NAMESPACE")
 
 DEST_NAMESPACE = f"{nessie_catalog_name}.{nessie_namespace}"
 PRIM_DATASET_URI = os.getenv("PRIM_DATASET_URI")
+PRIM_DATASET_TOKEN = os.getenv("PRIM_DATASET_TOKEN")
 DATASET = os.getenv("DATASET")
 DEST_TABLE = os.getenv("DESTINATION_TABLE")
 
 DATASET_URL = f"{PRIM_DATASET_URI}/{DATASET}/exports/csv"
 
-r = rq.get(f"{DATASET_URL}", timeout=30)
+headers = {
+    "Authorization": f"apikey {PRIM_DATASET_TOKEN}"
+}
+
+r = rq.get(f"{DATASET_URL}", headers=headers, timeout=30)
 r.raise_for_status()
 
 lines = r.content.decode("utf-8").splitlines()
+
 rdd = spark.sparkContext.parallelize(lines)
 
 df = (
